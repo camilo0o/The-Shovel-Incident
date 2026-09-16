a9219e79f924b17ebdfed172f5faa96bf92f1a1e
Dekyim <adrv97@gmail.com>
2026-09-16 19:32:12 -0300

    Terminado portal. Empezada la parte de Palin.

--- scenes/level_1.gd
+++ scenes/level_1.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-
-func _on_portal_body_entered(body: Node2D) -> void:
+func _on_portal_body_entered(body):
 	if body.name == "Player":
-		get_tree().change_scene_to_file("res://scenes/boss_room_1.tscn")
+		call_deferred("_cambiar_escena")
+
+func _cambiar_escena():
+	get_tree().change_scene_to_file("res://scenes/boss_room_1.tscn")
