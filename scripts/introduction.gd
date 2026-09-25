48ec66672ff24d310d6384015e446b1735c23f89
Camilo <ernesto.fagundez@estudiantes.utec.edu.uy>
2026-09-25 20:03:59 -0300

    agregado video introductorio y opcion de verlo en el menu

---
+++ scripts/introduction.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+const MAIN_MENU_PATH := "res://scenes/main_menu.tscn"
+
+@onready var video: VideoStreamPlayer = $VideoStreamPlayer
+
+func _ready() -> void:
+	video.finished.connect(_go_to_main_menu)
+	video.play()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		video.stop()
+		_go_to_main_menu()
+
+func _go_to_main_menu() -> void:
+	get_tree().change_scene_to_file(MAIN_MENU_PATH)
+
+# Opcional: conectá esto a un botón "Saltar" si le agregás uno a la escena
+func _on_skip_button_pressed() -> void:
+	video.stop()
+	_go_to_main_menu()
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -9,3 +9,6 @@ func _on_options_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_watch_intro_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/introduction.tscn")
